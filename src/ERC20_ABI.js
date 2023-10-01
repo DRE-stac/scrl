f707ccd1db1bb34812a485e0f01b6bb068d921bf
DRE-stac <81095893+DRE-stac@users.noreply.github.com>
2023-10-01 00:35:45 -0400

    Add files via upload

---
+++ src/ERC20_ABI.js
@@ -0,0 +1,27 @@
+const ERC20_ABI = [
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    // ... (other function entries)
+  ];
+  
